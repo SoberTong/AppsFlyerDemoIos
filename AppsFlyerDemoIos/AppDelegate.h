a64ab61de0fe9dae40f4b9296b8413e5adae45f6
Avidly <avidly@AvidlydeMac-mini.local>
2017-02-16 14:11:02 +0800

    Initial Commit

---
+++ AppsFlyerDemoIos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppsFlyerDemoIos
+//
+//  Created by Avidly on 2017/2/16.
+//  Copyright © 2017年 Avidly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppsFlyerDemoIosTests/AppsFlyerDemoIosTests.m
@@ -0,0 +1,39 @@
+//
+//  AppsFlyerDemoIosTests.m
+//  AppsFlyerDemoIosTests
+//
+//  Created by Avidly on 2017/2/16.
+//  Copyright © 2017年 Avidly. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppsFlyerDemoIosTests : XCTestCase
+
+@end
+
+@implementation AppsFlyerDemoIosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
